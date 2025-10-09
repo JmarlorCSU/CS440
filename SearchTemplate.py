4d35be57477b8b75d0e4ee5933ef339cca8abdc7
JmarlorCSU <justin.marlor@colostate.edu>
2025-10-09 08:55:48 -0600

    actions implemented, not debugged

--- SearchTemplate.py
+++ SearchTemplate.py
@@ -3,40 +3,79 @@ from queue import PriorityQueue, Queue
 
 class Problem(object):
     def __init__(self):
-        self.create_state_space()
         self.set_initial_state()
         self.create_actions()
-
-    def create_state_space(self):
-        raise NotImplementedError("Your code goes here")
+    
     def set_initial_state(self):
-        # This function should set self.initial_state
-        raise NotImplementedError("Your code goes here")
+        self.initial_state = [[8, 5, 3],
+                              [2, 1, 7],
+                              [4, 6, 0]
+                              ]
+        self.state = self.initial_state
 
     def create_actions(self):
-        raise NotImplementedError("Your code goes here")
+        self.row = [0, 0, -1, 1]
+        self.column = [-1, 1, 0, 0]
+        self.moves = ["left", "right", "up", "down"]
+    
     def goal_test(self, state):
-        raise NotImplementedError("Your code goes here")
-    def is_goal(self, state):
-        raise NotImplementedError("Your code goes here")
+        goal_state =  [[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 0]
+                       ]
+        return state == goal_state
+
+    def is_goal(self):
+        return self.goal_test(self.state)
+    
     def get_cost(self,action):
-        raise NotImplementedError("Your code goes here")
+        return 1
+    
     def heuristic(self, state, ucs_flag=False):
         if ucs_flag:
             return 0
         else:
-            self.your_heuristic_function(state)
+            return self.your_heuristic_function(state)
+
     def your_heuristic_function(self, state):
-        raise NotImplementedError("Your code goes here")
-
+        heuristic = 0
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                        number = state[i][j]
+                        if number != 0:
+                            goal_row = (number - 1) // 3 # 7 - 1 div 3 = 0
+                            goal_col = (number - 1) % 3 # 7 - 1 mod 3 = 0 
+                            heuristic += abs(i - goal_row) + abs(j - goal_col)
+                            # manhattan distance calculation between current and goal positions
+        return heuristic
+    
     def get_successors(self, state):
-        raise NotImplementedError("Your code goes here")
+        successor_list = []
 
+        # Return coordinates of blank space (i, j)
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if state[i][j] == 0:
+                    temp_i = i
+                    temp_j = j
+                    break
+
+        # calculate each move using offset vectors
+        for k in range(4):
+            # generate coordinates for each move 'left' 'right' 'up' 'down'
+            new_i = temp_i + self.row[k]
+            new_j = temp_j + self.column[k]
+            # check boundaries on coordinates
+            if  3 <= new_i <= 0 or 0 <= new_j >= 3:
+
+                state_copy = state[0:2, 0:2] # generate copy of parent state
+                state_copy[i][j], state_copy[new_i][new_j] = \
+                state_copy[new_i][new_j],  state_copy[i][j]
+                successor_list.append(state_copy)
 
 class Node(object):
     def __init__(self, state):
         ''' Feel free to add any additional arguments you need'''
-        self.state = state
         # You should add whatever fields you need here for your search
     def get_plan(self):
         ''' Return the plan to reach self from the start state'''
@@ -48,6 +87,8 @@ class Node(object):
 def astar_graph_search(problem, ucs_flag=False):
     start_state = Node(problem.initial_state)
     # Define fringe and closed list
+    path = [] # to track the states taken to reach the goal
+
     # It should return a Node object
     # You should pass the ucs_flag to the heuristic function
     # and you are only allowed to use the method problem.heuristic() in the search code.
